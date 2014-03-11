79f4832734416cbd714a08ac0d1fb66a75494ae7
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-11 16:18:10 -0700

    Problem 2 part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -13,3 +13,12 @@ var sparkyDogAge = sparkyAge * 7;
 
 console.log("Sparky is " + sparkyAge + " human years old which is " + sparkyDogAge + " in dog years.");
 
+//Problem 2: Slice of Pie Part 1
+
+var slicesPerPizza = 8;
+var partySize = 3;
+var pizzaTotal = 10;
+
+var slicesPerPerson = pizzaTotal * slicesPerPizza / partySize; 
+
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
