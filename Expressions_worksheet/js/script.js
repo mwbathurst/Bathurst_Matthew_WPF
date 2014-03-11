abbc3fc2de4309fb4bd64ead7d699985718eb4ec
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-11 16:40:02 -0700

    grocery average question

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -13,12 +13,28 @@ var sparkyDogAge = sparkyAge * 7;
 
 console.log("Sparky is " + sparkyAge + " human years old which is " + sparkyDogAge + " in dog years.");
 
-//Problem 2: Slice of Pie Part 1
+//Problem 2: Slice of Pie Part I
 
 var slicesPerPizza = 8;
-var partySize = 3;
-var pizzaTotal = 10;
-
-var slicesPerPerson = pizzaTotal * slicesPerPizza / partySize; 
+var partySize = 10;
+var pizzaTotal = 4;
+var totalSlices = pizzaTotal * slicesPerPizza;
+var slicesPerPerson = totalSlices / partySize; 
 
 console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+
+//Problem 3: Sliceof Pie Part II
+
+var sparkyPizzaTotal = totalSlices % partySize;
+
+console.log("Sparky got " + sparkyPizzaTotal + " slices of pizza.");
+
+
+//Problem 4: Average shopping bill
+
+var weeklyGrocery = [200, 450, 320, 150, 100];
+var totalGroceryCost = weeklyGrocery[0] + weeklyGrocery[1] + weeklyGrocery[2] + weeklyGrocery[3] + weeklyGrocery[4];
+var groceryAvg = totalGroceryCost / 5;
+
+console.log("You have spent a total of $" + totalGroceryCost + " on groceries over 5 weeks. That is an average of $" + groceryAvg + " per week");
+
