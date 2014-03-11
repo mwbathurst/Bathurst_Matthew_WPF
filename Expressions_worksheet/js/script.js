01a1a071a5b790cf207bfd020c7aa52e4abe1335
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-11 16:10:01 -0700

    problem 1 done

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -9,3 +9,7 @@ Web Fundamentals Online Section 3
 var sparkyAge = 4;
 var sparkyDogAge = sparkyAge * 7;
 
+//print's out the age of sparky in human and dog years to the console
+
+console.log("Sparky is " + sparkyAge + " human years old which is " + sparkyDogAge + " in dog years.");
+
