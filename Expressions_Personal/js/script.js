5ba7ec7253ad3fa9c093b81b2227796df178f61f
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-12 14:41:21 -0700

    Setup for Assignment 2

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,15 @@
+/* 
+Matthew Bathurst
+Assignment: Expressions (Personal File)
+Web Fundamentals Online Section 3
+*/
+
+/* This personal program will identify my protein consumption for an entire year, based upon gym standards
+*/
+
+// All the variable 
+var protein = prompt("What is the Protein type you use?"); //Identifies the Brand in which the user uses
+var workoutsPerWeek = prompt("How many times do you workout per week? on Average"); 
+var scoopsPerWorkout = prompt("How many Scoops of protein do you consume after a workout?");
+var gramsPerScoop = prompt("How many grams per scoop is there in " + protein + "?");
+console.log(gramsPerScoop);
---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,6 @@
+/* 
+Matthew Bathurst
+Assignment: Expressions (Wacky File)
+Web Fundamentals Online Section 3
+*/
+
