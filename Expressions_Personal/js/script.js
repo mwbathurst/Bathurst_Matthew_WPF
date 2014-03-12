6c1c5171c86e4cbba1e3c76eb5f3d32d723e80ed
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-12 15:32:10 -0700

    Stage 1: Variable Defining

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,7 +9,9 @@ Web Fundamentals Online Section 3
 
 // All the variable 
 var protein = prompt("What is the Protein type you use?"); //Identifies the Brand in which the user uses
-var workoutsPerWeek = prompt("How many times do you workout per week? on Average"); 
-var scoopsPerWorkout = prompt("How many Scoops of protein do you consume after a workout?");
-var gramsPerScoop = prompt("How many grams per scoop is there in " + protein + "?");
+var scoopsPerWorkout = prompt("How many Scoops of protein do you consume after a workout?"); // Identifies the Scoops per workout as this will help calculate the amount in which is consumed 
+var gramsPerScoop = prompt("How many grams per scoop is there in " + protein + "?");  // Identifies the size of a scoop for later calculation for total consumption
+var workoutsPerWeek = prompt("How many times do you workout per week? on Average");  // Identifies the frequence in which you consume protein
+
+
 console.log(gramsPerScoop);
