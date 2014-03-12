f7c83f9d95d18167c8a0c9cad55c16f7bdd2ae6d
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-12 16:28:30 -0700

    Working Assignment, Needs cleaning up, and annotations.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -15,10 +15,14 @@ var workoutsPerWeek = prompt("How many times do you workout per week on average?
 // Constants
 var weight = [1, 1000]; // weight[0] = grams per gram, weight[1] = grams per kilogram,
 var weeklyTotalScoops = scoopsPerWorkout * workoutsPerWeek //knowing how many workouts per week I know I'll consume how many scoops per week
-var yearTotalScoops = weeklyTotalScoops * 52 // yearly total of grams
-var weeklyGramsTotal = [weight[0] * weeklyTotalScoops, weeklyTotalScoops / weight[1] ];
-var yearlyGramsTotal = [weight[0] * yearTotalScoops, yearTotalScoops / weight[1] ];
+var yearTotalScoops = weeklyTotalScoops * 52 // yearly total of grams calculated
+var weeklyGramsTotal = [weight[0] * weeklyTotalScoops, weeklyTotalScoops / weight[1] ]; // organize and calculate weekly gram totals and kilogram total
+var yearlyGramsTotal = [weight[0] * yearTotalScoops, yearTotalScoops / weight[1] ];  //organize and calculate yearly gram total and kilogram total
+var howManyLbsPerYear = yearlyGramsTotal[1] * 2.2046;  // conversion of kg to lbs
 
 console.log(weeklyTotalScoops, yearTotalScoops);
-console.log(gramsTotal[0], gramsTotal[1]);
+console.log(yearlyGramsTotal[0], yearlyGramsTotal[1]);
+console.log(howManyLbsPerYear);
+
+console.log("You consume " + scoopsPerWorkout + " scoops of the Protein: "+ protein + "each workout.  You know that you workout, " +  workoutsPerWeek + " times a week.  From there you know you consume " + weeklyTotalScoops + " scoops per week.  At that rate you're consuming " + weeklyGramsTotal[0] + " grams, that amasses to " + yearlyGramsTotal[1] + " kilograms per year.  And when you go shopping for a years supply you know you need to purchase " + howManyLbsPerYear + " pounds of " + protein + "for the year!");
 
