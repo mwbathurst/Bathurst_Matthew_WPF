76196b8a4ce795c7708e43718af83878de20513f
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-12 21:50:15 -0700

    Stated the wacky problem

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,3 +4,10 @@ Assignment: Expressions (Wacky File)
 Web Fundamentals Online Section 3
 */
 
+/* You said wacky, I'm not to wacky of an individual, so a wacky calculation, how many lapdances 
+do we have to give to pay for my tuition.
+*/
+
+alert("Times are tough, that's why I decided to start dancing my through college.\nSo here's a calculator to figure out how many private dances you must perform to pay for school!");
+// This alerts the user to the problem at hand
+
