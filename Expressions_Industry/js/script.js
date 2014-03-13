ea37a7423e2e7f30ed36aa0c1df37adab85595b9
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-12 17:00:48 -0700

    Defined Industry Problem

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,3 +4,8 @@ Assignment: Expressions (Industry File)
 Web Fundamentals Online Section 3
 */
 
+/* I work for an MMA (Mixed Martial Arts) Gym, and one of the most presistent problems 
+I come across is how to explain the breakdown of a professional fight contract to a 
+fighter.  So I figured why not make a quick little program so I don't have to deal with
+them face to face.
+*/
