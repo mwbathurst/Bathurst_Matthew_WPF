9b3c121ff7cd0a6e30264126fcd8332d8cd65be9
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-12 22:06:30 -0700

    defining more variables with a check

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,9 +5,15 @@ Web Fundamentals Online Section 3
 */
 
 /* You said wacky, I'm not to wacky of an individual, so a wacky calculation, how many lapdances 
-do we have to give to pay for my tuition.
+do we have to give to pay for my tuition.  And before you ask
 */
 
 alert("Times are tough, that's why I decided to start dancing my through college.\nSo here's a calculator to figure out how many private dances you must perform to pay for school!");
 // This alerts the user to the problem at hand
 
+var tuition = prompt("How much tuition do you owe?"); // Asks the user to give valueable data to figure how they plan to pay it off
+var howMuch = prompt("How much will you charge for your dances?"); //prompt to get user precieved value
+var managementFee = howMuch / 2; // constant value for how much you owe your management
+var clubFee = howMuch / 8; //constant value for how much you owe the club
+
+console.log(managementFee, clubFee);
