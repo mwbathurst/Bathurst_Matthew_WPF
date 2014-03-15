d2863c901a9fe3e087844fc6b07cfebad4e184a3
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-15 15:21:13 -0700

    Group 1

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -16,4 +16,23 @@ if(competitorWeight > 250){
 }
 
 console.log(" --------- ");
-//
+
+//Group 1: Celsius to Fahrenheit Converter
+console.log("Celsius to Fahrenheit Converter");
+var temp = 100;
+var unit = "C";
+
+if(unit == "F"){
+	var tempA = 5/9 * (temp - 32);
+	console.log("The Temperature is " + tempA + " degrees Celsius.");
+;}else if(unit == "C"){
+	var tempA = (9/5 * temp) + 32;
+	console.log("The Temperature is " + tempA + " degrees Fahrenheit.");
+}else{
+	console.log("We're only using Fahrenheit & Celsius units of measure for this!");
+}
+
+console.log(" --------- ");
+
+
+
