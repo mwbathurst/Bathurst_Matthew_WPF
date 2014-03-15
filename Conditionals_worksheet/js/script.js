493dc5d37a0909f5faa9ef40d3818743a7b610b3
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-15 11:35:19 -0700

    part 1 done

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,3 +4,16 @@ Matthew Bathurst
 Activity: Conditionals Worksheet
 Web Fundamentals Online Section 3
 */
+
+//STUFF YOUR FACE I
+console.log("Stuff Your Face I");
+var competitorWeight = 245;
+
+if(competitorWeight > 250){
+	console.log("The competitor qualifies for the heavyweight division.");
+}else{
+	console.log("The competitor needs to gain some weight!");
+}
+
+console.log(" --------- ");
+//
