f3baa148051e8f1d6146e30ddca34a949414a75a
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-16 13:56:15 -0700

    Define Formula for Equation

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,6 +6,10 @@ March 15, 2013
 */
 
 /*
-Industry Problem: How much water do I have to consume to make sure i stay 
+Industry Problem: How much water do I have to consume to make sure I stay 
 hydrate on the average day.  
 */
+
+/* Formula = weight * 2/3 + (12 * (workout time/60min))
+*/
+
