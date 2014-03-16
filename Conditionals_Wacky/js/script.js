83f635df2b5a310e15c4bc34187d9d7a1e57e461
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-15 21:25:59 -0700

    Define Wacky Problem for Conditional Assignment

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,3 +4,8 @@ Assignment: Conditional Wacky
 Web Fundamentals Online Section 3
 March 15, 2013
 */
+
+/*
+Wacky Conditional Problem: Who is more of a man, Wilt Chamerblain or you?
+I got the idea of this from reading Wilt's book, and his exploits.  
+*/
