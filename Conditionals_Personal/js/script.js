1eaf91455a33502a74cd21a6185cae4ac2b48958
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-16 12:26:34 -0700

    Step 1 in Developing code for Personal Conditional

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,7 +5,14 @@ Web Fundamentals Online Section 1
 March 15, 2013
 */
 
-/*
-Question needing calculating: Have you earned the right to 
-consume desert tonight? 
+
+// Question needing calculating: Have you earned the right to consume protein pudding tonight? 
+
+
+/* Basic Health guidelines, I prescribe to in terms of food.  I consume 1.0 - 1.5g of Protein per lb of weight per day.  
+If this dietary requirement is not meet I eat a protein pudding, if I exceed 1.5g of Protein per lb, I do not consume the protein puddying.
 */
+
+alert("Let's calculate if you deserve to have protein pudding for desert tonight.");
+
+
