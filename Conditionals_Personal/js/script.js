7081e1464a88b1cc5ab3f91ee9905b908873c7d3
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-16 12:57:10 -0700

    Simplified Question back to not to include gender roles, as it's a personal problem

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,17 +11,19 @@ March 15, 2013
 
 /* Basic Health guidelines, I prescribe to in terms of food.  I consume 1.5g of Protein per lb of weight per day.  
 If this dietary requirement is not meet I eat a protein pudding, if I exceed 1.5g of Protein per lb, I do not consume the protein puddying.
-I will also create variables so that a user with specific dietary goals in terms of protein can change for personal settings.
 */
 
 alert("Let's calculate if you deserve to have protein pudding for desert tonight."); //Introduction to the Program
 
 //Variable List
+
+
 var weight = prompt("How many pounds do you weigh?"); // Defines the 1st input from the user that will be basis for determining calculation for solution 
 var protein = prompt("How many grams of protein have you consumed today"); // Defines the 2nd input from the user that will help determine calculation for solution
 var proteinPerLb = protein / weight; // Main Calculation for Problem 
-var myProteinPerLb = 1.5; // This is a variable that is customizable for the user
+var maleProteinPerLb = 1.5; // This is a gender specific comparision calculation for males
 var desert; // Setting up for Ternary statement of Protein/PerLb > 1.5g No Protein Pudding Desert, if Protein < 1.5g then consume protein pudding
+var voidInput = ""; // Setup for Validating inputs from user
 
 
 
