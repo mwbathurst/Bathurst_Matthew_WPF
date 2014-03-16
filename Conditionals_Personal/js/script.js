c5a15d367cddf36cbe20d8dcc0dd9c5353332c28
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-15 21:14:41 -0700

    Defined the Personal Problem for the Conditional Assignment

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,3 +4,8 @@ Assignment: Conditional Personal
 Web Fundamentals Online Section 3
 March 15, 2013
 */
+
+/*
+Question needing calculating: Have you earned the right to 
+consume desert tonight? 
+*/
