359f08a1854bc0b63f06194f198d64bdce1fffca
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-16 14:29:59 -0700

    Waiting for Class on Wed to ask about validations to finish script

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -21,12 +21,23 @@ var workout = prompt("How many minutes do you workout per day?");
 var consumption = prompt("How many ounces of water have you consumed today?");
 var output = weight * 2/3 + 12 * workout/60; 
 // Final Calculation for comparision as it's formula defines required fluid; 
-if(consumption => output && consumption < 200){
+
+if(consumption >= output && consumption < 200){
 	console.log("Your Daily goal of " + output + " ounces of water has been meet. \nRemember over-hydration can be just as dangerous as under-hydration, so remember to stay under 200 ounces a day." );
 	// this is the print out if consumption is greater than Output and less than medical max of 200
-}else if (consumption => 200){
+}else if (consumption >= 200){
 	console.log("You've reached the level of over-hydration for the day, please do not consume any more liquids today.");
-	//defines the variable 
+	//defines that you have over consumed for the day
 }else{
-
+	var finalDif = output - consumption
+// variable if the if statement is false and allows
+	if(output > 200){
+		console.log("You've consumed " + consumption + " ounces today.  You need to consume " + Number(200 - consumption) + " ounces more today to reach your daily goal." );
+		//if consumption goal wasn't meet, print out the total consumed and what is still needed to consume for the day(if the raw consumption goal was greater than 200)
+	}else{
+			console.log("You've consumed " + consumption + " ounces today.  You need to consume " + finalDif + " ounces more today to reach your daily goal." );
+			//if consumption goal wasn't meet, print out the total consumed and what is still needed to consume for the day
+	}
+	// If/else if output total was greater than 200 and since that breaks the rule of max consumption, thus was set to cap usage.		
 }
+	//nested if statements to print out goal in which you need to consume.  
