6ad095b2cf6a19d1aeeb3846bdf85ee153a553ca
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-15 17:31:21 -0700

    Final File

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -34,5 +34,35 @@ if(unit == "F"){
 
 console.log(" --------- ");
 
+//Group 2: Check the Login
+console.log("Check the Login");
+
+//user inputs 
+var username = "Admin";
+var password = "password";
+
+//correct inputs
+var usernameMaster = "Admin";
+var passwordMaster = "password";
+
+if (username == usernameMaster && password == passwordMaster) {
+	console.log("Welcome, " + username + "!");	
+}else if(username !== usernameMaster){
+	console.log("User not found. Try again.");
+}else{
+	console.log("Password does not match our records.");
+}
+
+console.log(" --------- ");
+
+// Group 3  Tire Pressure
+console.log("Tire Pressure");
+var tirePressure = [10, 10, 20, 20]; //[Left Front Tire, Right Front Tire, Left Back Tire, Right Back Tire]
+
+if(tirePressure[0] == tirePressure[1] && tirePressure[2] == tirePressure[3]) {
+	console.log("The tires pass spec!");
+}else{
+	console.log("Get your tires checked out!");
+}
 
 
