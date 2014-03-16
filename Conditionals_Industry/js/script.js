269f915f3979913ddaa5a86c9c2e576c60ff2649
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-15 21:19:33 -0700

    Define the Conditional Problem for my Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,8 @@ Assignment: Conditional Industry
 Web Fundamentals Online Section 3
 March 15, 2013
 */
+
+/*
+Industry Problem: How much water do I have to consume to make sure i stay 
+hydrate on the average day.  
+*/
