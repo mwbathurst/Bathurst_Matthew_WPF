e22833811ac2e34c18991a990375f62eb4be7170
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-19 13:03:00 -0700

    Before Final Cleanup

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -12,7 +12,7 @@ hydrate on the average day.
 
 /* Formula in ounce= weight * 2/3 + (12oz * (workout time/60min)) 
 	if output is greater then 200 out, then it's capped at 200
-*/
+
 alert("Welcome to your Personal Hydration Monitoring System.");
 var weight = prompt("How much do you weight?");
 //asks users to input first important factor into calculation
@@ -41,3 +41,66 @@ if(consumption >= output && consumption < 200){
 	// If/else if output total was greater than 200 and since that breaks the rule of max consumption, thus was set to cap usage.		
 }
 	//nested if statements to print out goal in which you need to consume.  
+	*/
+
+
+
+alert("Welcome to the Personal Hydration Calculation System. How much water do you need to drink today?");
+//Lets the User Know the software they are about to User
+var weight = prompt("How much do you weight?");
+//asks users to input first important factor into calculation
+if(weight == ""){
+	//if validation for weight input
+	var weight = prompt("Please ENTER your weight now.");
+	var workout = prompt("How many minutes do you workout per day?");
+	if(workout == ""){
+		//if validation for workout input
+		var workout = prompt("Please how many minutes do you workout per day?")
+		// asks user to input second important factor into calculation
+		var output = weight * 2/3 + 12 * workout/60; 
+		// Final Calculation for comparision as it's formula defines required fluid; 
+		var cap = 200;
+		// Max Cap for user to consume
+		var consumption;
+		consumption = (output >= cap) ? "You've reached the medical advised amount of water consumption of " + cap + " ounces per day" : "Your advised amount of water consumption is " + output + " ounces per day.  Please remember do not exceed " + cap + " ounces per day.";
+		//tenary statement for output statement
+		console.log(consumption);
+	}else{
+		//else validation for workout input
+		var output = weight * 2/3 + 12 * workout/60; 
+		// Final Calculation for comparision as it's formula defines required fluid; 
+		var cap = 200;
+		// Max Cap for user to consume
+		var consumption;
+		consumption = (output >= cap) ? "You've reached the medical advised amount of water consumption of " + cap + " ounces per day" : "Your advised amount of water consumption is " + output + " ounces per day.  Please remember do not exceed " + cap + " ounces per day.";
+		//tenary statement for output statement
+		console.log(consumption);
+	}
+}else{
+	//else validation for weight input
+	var workout = prompt("How many minutes do you workout per day?");
+	// asks user to input second important factor into calculation
+	if(workout == ""){
+		//if validation for workout input
+		var workout = prompt("Please how many minutes do you workout per day?")
+		// asks user to input second important factor into calculation
+		var output = weight * 2/3 + 12 * workout/60; 
+		// Final Calculation for comparision as it's formula defines required fluid; 
+		var cap = 200;
+		// Max Cap for user to consume
+		var consumption;
+		consumption = (output >= cap) ? "You've reached the medical advised amount of water consumption of " + cap + " ounces per day" : "Your advised amount of water consumption is " + output + " ounces per day.  Please remember do not exceed " + cap + " ounces per day.";
+		//tenary statement for output statement
+		console.log(consumption);
+	}else{
+		//else validation for workout input
+		var output = weight * 2/3 + 12 * workout/60; 
+		// Final Calculation for comparision as it's formula defines required fluid; 
+		var cap = 200;
+		// Max Cap for user to consume
+		var consumption;
+		consumption = (output >= cap) ? "You've reached the medical advised amount of water consumption of " + cap + " ounces per day" : "Your advised amount of water consumption is " + output + " ounces per day.  Please remember do not exceed " + cap + " ounces per day.";
+		//tenary statement for output statement
+		console.log(consumption);
+	}
+}
