cd2c6c5f1c968ba90ae7537578cdf5d62a48a7d4
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 12:25:43 -0700

    Defined the Problem

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,7 +6,8 @@ Date: Week 4
 */
 
 
-/* The calculation for my job.  Max Heart calculation
+/* The calculation for my job.  Max Heart calculation for personal training clients
+for max efficent work calculations
 if male: 202 - (.55 x age)
 else female 216 -(1.09 x age)
 
