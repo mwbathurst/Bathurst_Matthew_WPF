68af129acd67390d67925303855babdacbc406f3
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 12:13:39 -0700

    Defined Question for Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,3 +4,11 @@ Assignment: Functions (Industry File)
 Web Fundamentals Online Section 1
 Date: Week 4 
 */
+
+
+/* The calculation for my job.  Max Heart calculation
+if male: 202 - (.55 x age)
+else female 216 -(1.09 x age)
+
+inputs = gender & age
+*/
