5506a78940d7ec7f7d5fbdb88fedf8b710b01838
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 12:29:57 -0700

    Defined Problem

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,3 +4,9 @@ Assignment: Functions (Personal File)
 Web Fundamentals Online Section 1
 Date: Week 4 
 */
+
+/*
+How much should I expect to pay on a date with a woman, based completely on looks.
+The 3 inputs: Her "Rating" from 1-10, my "Rating" from 1-10, and how much is in my bank account
+*/
+
