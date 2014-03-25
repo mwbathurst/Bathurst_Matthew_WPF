3427c725bd65c296109e8b56bf2784547f8affa4
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 12:26:27 -0700

    Define the Problem for Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,3 +4,15 @@ Assignment: Functions (Wacky File)
 Web Fundamentals Online Section 1
 Date: Week 4 
 */
+
+/*
+How many shots of whiskey does it take to get blackout drunk? I find this to be 
+a wacky question as I live a straight edge life style, and always
+wondered how much it takes.
+We will be working with the concept of .30 Blood Alcohol level as the "blackout" threshold 
+as that's what I remember from the lecture I got as a college
+Freshman in 2001. 
+*/
+
+
+
