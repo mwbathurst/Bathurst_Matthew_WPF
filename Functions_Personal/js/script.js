4939c7648a3bcaa82de4ddb5240529fe0011cfa2
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 13:22:55 -0700

    Define first set of variables

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,7 +6,20 @@ Date: Week 4
 */
 
 /*
-How much should I expect to pay on a date with a woman, based completely on looks.
-The 3 inputs: Her "Rating" from 1-10, my "Rating" from 1-10, and how much is in my bank account
+How much should I expect to pay on a date with a woman, based completely on looks and age?
+The 3 inputs: Her "Rating" for any numerical scale, my "Rating" for any numerical scale, and how old is she.
+This calculation can be attributed to Keith Aragon baseball coach from Fort Collins High School
+
+if she is => 21 Her rating * $15 - your rating * $1.75
+else her rating * $10 - your rating * 1.75
 */
+
+alert("How much will a date with this woman cost you?");
+//Alerts the User to What the Calculation is for
+
+var herRating = prompt("Rate your Date, give your date a numerical rating."); // This prompts the user for an input on his/her date, can be any number
+var yourRating = prompt("Rate yourself, give yourself a numerical rating."); // This prompts the user for an input on his/her own rating, can be any number
+var herAge = prompt("How old is your date?"); // This prompts the user for an input on his/her date's age as this will modify the calculation for potential alcohol consumption
+
+
 
