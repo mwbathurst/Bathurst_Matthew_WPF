2eec2121c723bc410fdb9006b5f4147651f6eacc
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 19:48:15 -0700

    Tested Pre-Prompt and Commented

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -14,12 +14,29 @@ alert("Is tonight the night to ask that special someone to marry you?");
 //alerts the user to the question at hand
 
 //equation defined bank account / (cost of ring * 2) = the One Ring Factor
+// I was always taught you need to have 1.5 times the cost of 2 ring in your bank account to purchase it and with my personal feelings on credit cards 
+// I believe you can purchase it on credit if you have at least more then the cost of 2 rings
 
 var ringCost = 210;
-var bankAccount = 100;
-var theOneRing = bankAccount / ringCost * 2;
-console.log(theOneRing);
- 
+var bankAccount = 421;
+var ring = ring(ringCost, bankAccount);
+
+
+if(ring >= 1.5){
+	console.log("You can easily afford that ring! Your bank account can afford, so pay cash for it and ask that person tonight!!");
+}else if(ring <=1.5 && ring > 1){
+	console.log("You have enough money to purchase the ring, but knowing Old Man Bathurt, I would suggest buying the ring on credit and purposing tonight!")
+}
+else{
+	console.log("Sorry you're to broke, don't ask her to marry that special someone unless they are ok with a crackerjack ring.");
+}
+
+function ring(cost, account){
+	var theOneRing = account / (cost * 2);
+	return theOneRing;
+
+}
+
 
 
 
