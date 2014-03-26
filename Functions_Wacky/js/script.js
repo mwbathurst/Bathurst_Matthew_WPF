8b04b489a3435ab60d13ca8f335bcfa5d0c89d88
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 19:51:13 -0700

    Pre-Commented software

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -17,15 +17,14 @@ alert("Is tonight the night to ask that special someone to marry you?");
 // I was always taught you need to have 1.5 times the cost of 2 ring in your bank account to purchase it and with my personal feelings on credit cards 
 // I believe you can purchase it on credit if you have at least more then the cost of 2 rings
 
-var ringCost = 210;
-var bankAccount = 421;
+var ringCost = prompt("Hey you're ready to ask that special someone.\nSo how much does that ring cost?");
+var bankAccount = prompt("Old Man Bathurst use to say gotta have money in the bank to pay for it. So using his super secret formula, He needs to know how much you got in your Bank Account.");
 var ring = ring(ringCost, bankAccount);
-
 
 if(ring >= 1.5){
 	console.log("You can easily afford that ring! Your bank account can afford, so pay cash for it and ask that person tonight!!");
 }else if(ring <=1.5 && ring > 1){
-	console.log("You have enough money to purchase the ring, but knowing Old Man Bathurt, I would suggest buying the ring on credit and purposing tonight!")
+	console.log("You have enough money to purchase the ring, but knowing Old Man Bathurt, I would suggest buying the ring on credit and proposing tonight!")
 }
 else{
 	console.log("Sorry you're to broke, don't ask her to marry that special someone unless they are ok with a crackerjack ring.");
