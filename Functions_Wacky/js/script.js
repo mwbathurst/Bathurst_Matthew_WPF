52f2d90bc21dfe6a6739856ba9fb863744eed675
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 18:42:40 -0700

    Define Problem

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,5 +5,15 @@ Web Fundamentals Online Section 1
 Date: Week 4 
 */
 
+/* Question: Can I propose tonight? Why is this wacky? Cause honestly marriage is scary 
+Solution is a ratio of cost of the ring * 2 compared to 
+available funds in bank account
+*/
+
+alert("Is tonight the night to ask that special someone to marry you?");
+//alerts the user to the question at hand
+
+
+
 
 
