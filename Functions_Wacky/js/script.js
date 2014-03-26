5d093b81486bbcee81612e5033c755acf48076ba
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-03-25 20:00:34 -0700

    Final Wacky Calculation

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -18,24 +18,31 @@ alert("Is tonight the night to ask that special someone to marry you?");
 // I believe you can purchase it on credit if you have at least more then the cost of 2 rings
 
 var ringCost = prompt("Hey you're ready to ask that special someone.\nSo how much does that ring cost?");
+// prompt for ring cost for calculation
 var bankAccount = prompt("Old Man Bathurst use to say gotta have money in the bank to pay for it. So using his super secret formula, He needs to know how much you got in your Bank Account.");
+// prompt for bank account info for calculation
 var ring = ring(ringCost, bankAccount);
+//defines the variable for the program to compare after running the ring function
 
 if(ring >= 1.5){
+	//if statement that compares the ring value to the threshold set by Old Man Bathurst
 	console.log("You can easily afford that ring! Your bank account can afford, so pay cash for it and ask that person tonight!!");
-}else if(ring <=1.5 && ring > 1){
-	console.log("You have enough money to purchase the ring, but knowing Old Man Bathurt, I would suggest buying the ring on credit and proposing tonight!")
+	// output to console that the if part is true
+	}else if(ring <=1.5 && ring > 1){
+		// else if statement that compares the ring value to the threshold set for Credit Purchase
+		console.log("You have enough money to purchase the ring, but knowing Old Man Bathurt, I would suggest buying the ring on credit and proposing tonight!")
+		// output to the console if the else if is true
+	}else{
+		// else section of if statement, that generates the result of everything being false
+		console.log("Sorry you're too broke, don't ask her to marry that special someone unless they are ok with a crackerjack ring.");
+		//output to console if everything is false 
 }
-else{
-	console.log("Sorry you're to broke, don't ask her to marry that special someone unless they are ok with a crackerjack ring.");
-}
-
+//the main program that decides if you can buy the ring
 function ring(cost, account){
-	var theOneRing = account / (cost * 2);
-	return theOneRing;
+	var theOneRing = account / (cost * 2); // calculation of the parameters of the function  
+	return theOneRing; // returns a value for future reference
 
-}
-
+} //Function that calculates the Old Man Bathurst Formula 
 
 
 
